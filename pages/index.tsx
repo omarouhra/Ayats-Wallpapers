2c6388a12cae7d9be0f1fed7fe8ac3fe973fb35a
omarouhra <omarmaroc26@gmail.com>
2022-08-21 16:28:53 +0100

    feat: hero section

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,8 +14,13 @@ const Home = () => {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <main>
-        <Title title="Make your wallpapers reminds you of god" />
-        <Text text="t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using" />
+        <section className="py-12">
+          <Title title="Make your wallpapers reminds you of god" />
+          <Text text="t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using" />
+          <button className="font-light border-[1.7px] rounded-sm dark:hover:text-black hover:text-white border-gray-900 dark:border-gray-200 py-2 px-5 hover:bg-gray-900 dark:hover:bg-gray-100 ">
+            Choose your prefered Aya  🕌
+          </button>
+        </section>
       </main>
     </div >
   );
