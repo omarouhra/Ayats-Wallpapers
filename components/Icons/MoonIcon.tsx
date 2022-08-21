c99c72d2481c6900475838942e6c4e67eaf8a11a
omarouhra <omarmaroc26@gmail.com>
2022-08-21 17:21:59 +0100

    fix: build error

--- components/Icons/MoonIcon.tsx
+++ components/Icons/MoonIcon.tsx
@@ -1,4 +1,6 @@
-const MoonIcon = () => {
+import React from 'react'
+
+function MoonIcon() {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
@@ -6,7 +8,7 @@ const MoonIcon = () => {
       fill='none'
       viewBox='0 0 24 24'
       stroke='currentColor'
-      strokeWidth={2}
+      strokeWidth={ 2 }
     >
       <path
         strokeLinecap='round'
@@ -14,7 +16,7 @@ const MoonIcon = () => {
         d='M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z'
       />
     </svg>
-  );
-};
+  )
+}
 
-export default MoonIcon;
+export default MoonIcon
