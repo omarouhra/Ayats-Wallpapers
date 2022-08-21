87dec9174c0e6551fa6aa0541f422fcc553b15c3
omarouhra <omarmaroc26@gmail.com>
2022-08-21 19:41:11 +0100

    fix: build error

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -1,9 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
 import Logo from "../Logo";
-import MoonIcon from "../icons/MoonIcon";
-import SunIcon from "../icons/SunIcon";
-
 function Header() {
   const { resolvedTheme, theme, setTheme } = useTheme();
 
@@ -20,7 +17,33 @@ function Header() {
           className='rounded-md p-2 hover:bg-gray-200 dark:hover:bg-gray-700'
           aria-label={ resolvedTheme === "light" ? "Dark Mode" : "Light Mode" }
         >
-          { theme === "light" ? <MoonIcon /> : <SunIcon /> }
+          { theme === "light" ? <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className='h-6 w-6'
+            fill='none'
+            viewBox='0 0 24 24'
+            stroke='currentColor'
+            strokeWidth={ 2 }
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z'
+            />
+          </svg> : <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className='h-6 w-6'
+            fill='none'
+            viewBox='0 0 24 24'
+            stroke='currentColor'
+            strokeWidth={ 2 }
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z'
+            />
+          </svg> }
         </button>
       </div>
     </header>
