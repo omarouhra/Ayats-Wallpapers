db54e770441da28ba30fb75b254001b6bb26ad1a
omarouhra <omarmaroc26@gmail.com>
2022-08-24 00:47:52 +0100

    feat: update verses

--- data/verses.js
+++ data/verses.js
@@ -11,18 +11,18 @@ const VERSES = [
     verseAr: "لَا تَحْزَنْ إِنَّ ٱللَّهَ مَعَنَا",
     verseEn: "Do not worry; Allah is certainly with us.",
   },
-  {
-    source: "Aya 58, Chapter Al-Furqan",
-    verseAr:
-      "وَتَوَكَّلْ عَلَى ٱلْحَىِّ ٱلَّذِى لَا يَمُوتُ وَسَبِّحْ بِحَمْدِهِۦ ۚ وَكَفَىٰ بِهِۦ بِذُنُوبِ عِبَادِهِۦ خَبِيرًا",
-    verseEn:
-      "Put your trust in the Ever-Living, Who never dies, and glorify His praises. Sufficient is He as All-Aware of the sins of His servants.",
-  },
-  {
-    source: "Aya 5, Chapter Ash-Sharh",
-    verseAr: "فَإِنَّ مَعَ ٱلْعُسْرِ يُسْرًا",
-    verseEn: "So, surely with hardship comes ease.",
-  },
+  // {
+  //   source: "Aya 58, Chapter Al-Furqan",
+  //   verseAr:
+  //     "وَتَوَكَّلْ عَلَى ٱلْحَىِّ ٱلَّذِى لَا يَمُوتُ وَسَبِّحْ بِحَمْدِهِۦ ۚ وَكَفَىٰ بِهِۦ بِذُنُوبِ عِبَادِهِۦ خَبِيرًا",
+  //   verseEn:
+  //     "Put your trust in the Ever-Living, Who never dies, and glorify His praises. Sufficient is He as All-Aware of the sins of His servants.",
+  // },
+  // {
+  //   source: "Aya 5, Chapter Ash-Sharh",
+  //   verseAr: "فَإِنَّ مَعَ ٱلْعُسْرِ يُسْرًا",
+  //   verseEn: "So, surely with hardship comes ease.",
+  // },
 ];
 
 export { VERSES };
