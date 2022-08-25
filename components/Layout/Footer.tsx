e8043635b1558cfbd1756b79552af91e920cd6b7
omarouhra <omarmaroc26@gmail.com>
2022-08-25 16:36:18 +0100

    feat: footer content

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -5,9 +5,9 @@ function Footer() {
   return (
     <footer className='py-8 flex flex-col md:flex-row items-start justify-between space-y-12 md:space-y-0' >
       <div className="flex space-x-4 justify-start w-full max-w-xs ">
-        <IconLink label="Github" />
-        <IconLink label="Twitter" />
-        <IconLink label="Linkedin" />
+        <IconLink label="Github" href="https://github.com/omarouhra" />
+        <IconLink label="Twitter" href='https://twitter.com/OuhraOmar' />
+        <IconLink label="Linkedin" href="https://www.linkedin.com/in/omar-ouhra/" />
       </div>
       <p className="text-gray-500 dark:text-gray-100 ">Built & designed by <strong>Omar Ouhra</strong>  in 🇲🇦 </p>
     </footer >
--- components/core/IconLink.tsx
+++ components/core/IconLink.tsx
@@ -3,14 +3,14 @@ import Link from 'next/link'
 
 type LinkType = {
     label: string;
-    href?: string;
+    href: string;
     className?: string;
 }
 
 function IconLink({ label, href, className }: LinkType) {
     return (
-        <Link href='/' >
-            < a >
+        <Link href={href} >
+            < a target='_blank' >
                 <div className='text-gray-400 font-light hover:text-black dark:text-gray-200 dark:hover:text-white'>
                     <span>
                         { label }
