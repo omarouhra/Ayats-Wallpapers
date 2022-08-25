ba3588bb5cb2823ecdb94a7e744d2777d3a65a39
omarouhra <omarmaroc26@gmail.com>
2022-08-25 22:18:19 +0100

    fix: build error

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ import { WALLPAPERS } from '../data/wallpapers'
 import useSWR from "swr";
 import { LinkedinShare, TwitterShare } from "../components/Share";
 import pic from '../public/preview/v1/p1.png'
-import { NextSeo } from 'next-seo';
+
 
 
 
