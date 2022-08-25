ae3180dffea1be2b614721ab004f8bf64b69226e
omarouhra <omarmaroc26@gmail.com>
2022-08-25 16:25:43 +0100

    feat: modal wallpaper layout

--- components/Wallpaper.tsx
+++ components/Wallpaper.tsx
@@ -47,7 +47,7 @@ function Wallpaper({ imgSrc, alt, ayaData }: ImageProps) {
         setReplay(true)
         setTimeout(() => {
             setReplay(false)
-        }, 2000);
+        }, 3000);
 
     }
 
@@ -71,25 +71,31 @@ function Wallpaper({ imgSrc, alt, ayaData }: ImageProps) {
 
                         setTimeout(() => {
                             setShowPreviewModal(false)
-                        }, 1000);
+                        }, 3000);
 
                     } }
                     className='inline-block w-full my-12  space-y-6 max-w-[1200px] py-8 px-5  overflow-hidden text-center align-middle transition-all bg-white dark:bg-[#023E51] shadow-xl rounded-lg'>
 
-                    <a download='wallpaper' href={ `/wallpapers/${alt}.zip` } className=' group  w-full  flex flex-col space-y-4 group transition duration-500 '>
+                    <div className=' group  w-full  flex flex-col space-y-4 group transition duration-500 '>
                         <Image src={ imgSrc } alt={ alt } className='rounded-md' />
 
                         <div className='flex items-center justify-between space-x-2'>
 
-                            <span className=' dark:text-black group-hover:text-[#04ade0] group-hover:translate-x-2 tranistion duration-300'>Click to Downloads</span>
-                            <div className='flex space-x-4 items-center'>
+                            <a download='wallpaper' href={ `/wallpapers/${alt}.zip` } className=' outline-none bg-gray-300 dark:text-black p-2 rounded-md text-xs font-semibold group-hover:text-[#04ade0]  tranistion duration-300'>
+                                <div className='flex items-center space-x-4'>
+                                    <span>
+                                        Click to Downloads
+                                    </span>
+                                    <DownloadIcon />
+                                </div>
+                            </a>
 
-                                <DownloadIcon />
-                                <span className={ `bg-gray-300 dark:text-black p-2 rounded-md text-xs font-semibold   transition duration-400 ${replay && 'bg-[#04ade0] text-white scale-[1.3] translate-y-3 '} ` } > { data && data[0]?.downloads } </span>
+                            <div className='flex space-x-4 items-center'>
+                                <span className={ `bg-gray-300 dark:text-black p-2 rounded-md text-xs font-semibold   transition duration-400 ${replay && 'bg-[#04ade0] text-white scale-[1.3] translate-y-3 '} ` } > { data && data[0]?.downloads } downloads </span>
                             </div>
 
                         </div>
-                    </a >
+                    </div>
                 </div>
             </Modal>
         </>
